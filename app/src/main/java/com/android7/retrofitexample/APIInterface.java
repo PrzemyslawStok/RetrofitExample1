de2611229802af45fcff8ab57545a8ec9c06f5e9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-04-07 17:26:22 +0200

    cd..

--- app/src/main/java/com/android7/retrofitexample/APIInterface.java
+++ app/src/main/java/com/android7/retrofitexample/APIInterface.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface APIInterface {
     @GET("/simpleFunction")
     Call<List<Data>> getSimpleFunction();
+
+    @GET("/squareFunction")
+    Call<List<Data>> getSquareFunction(@Query("A") float A, @Query("B") float B, @Query("C") float C);
 }
--- app/src/main/java/com/android7/retrofitexample/MainActivity.java
+++ app/src/main/java/com/android7/retrofitexample/MainActivity.java
@@ -5,7 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.List;
 
@@ -13,35 +18,58 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+
 public class MainActivity extends AppCompatActivity {
 
     APIInterface apiInterface;
+    GraphView mGraph;
+
+    Button mReloadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mGraph =  findViewById(R.id.graph);
         apiInterface = APIClient.getClient().create(APIInterface.class);
-
-        Call<List<Data>> dataList = apiInterface.getSimpleFunction();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        dataList.enqueue(new Callback<List<Data>>() {
+        mReloadButton = findViewById(R.id.button);
+
+        mReloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<List<Data>> call, Response<List<Data>> response) {
-                List<Data> data = response.body();
+            public void onClick(View v) {
+                Call<List<Data>> dataSquareFunction = apiInterface.getSquareFunction(1,0,0);
 
-                Toast.makeText(MainActivity.this,data.toString(),Toast.LENGTH_LONG).show();
-            }
+                dataSquareFunction.enqueue(new Callback<List<Data>>() {
+                    @Override
+                    public void onResponse(Call<List<Data>> call, Response<List<Data>> response) {
+                        List<Data> dataList = response.body();
 
-            @Override
-            public void onFailure(Call<List<Data>> call, Throwable t) {
+                        LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
 
+                        double x = 0;
+
+                        for(Data data : dataList){
+                            series.appendData(new DataPoint(x,data.number),true,100,true);
+                            x+=1;
+                        }
+
+                        mGraph.addSeries(series );
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Data>> call, Throwable t) {
+
+                    }
+                });
             }
         });
+
+
     }
 
     @Override
